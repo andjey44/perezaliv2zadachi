680cfb48231fcc80801bbf82862f9da9234224c2
andjey44 <andjeykh@yandex.ru>
2020-04-12 15:54:49 +0300

    calc all

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,49 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-      public static void main(String args[]) {
-            int n, arr[];
-            Scanner in = new Scanner(System.in);
-            System.out.println("Введите размер массива");
-            n = in.nextInt();
-            arr = new int[n];
-            for (int i : arr)
-                  i = (int) (Math.random() * n);
-            for (int i : arr)
-                  System.out.print(i + " ");
-      }
-}
+
+
+                  public static void main(String args[]) {
+                        int firstnumber, secondnumber, choice;
+
+                        System.out.println("1- Add");
+                        System.out.println("2- Sub");
+                        System.out.println("3- Div");
+                        System.out.println("4- Mul");
+                        System.out.print("Enter your choice -");
+                        Scanner var = new Scanner(System.in);
+
+                        choice = var.nextInt();
+                        System.out.print("Enter first number -");
+                        firstnumber = var.nextInt();
+                        System.out.print("Enter second number -");
+                        secondnumber = var.nextInt();
+                        calc(choice, firstnumber, secondnumber); // 3rd arg added for choice
+                  }
+
+                  public static void calc(int choice, int x, int y) { // 3rd param added for choice
+                        int c;
+
+                        switch (choice) {
+                              case 1:
+                                    c = x + y;
+                                    System.out.print("Output-" + c);
+                                    break;
+
+                              case 2:
+                                    c = x - y;
+                                    System.out.print("Output-" + c);
+                                    break;
+
+                              case 3:
+                                    c = x / y;
+                                    System.out.print("Output-" + c);
+                                    break;
+
+                              case 4:
+                                    c = x * y;
+                                    System.out.print("Output-" + c);
+                                    break;
+                        }
+                  }
+            }
