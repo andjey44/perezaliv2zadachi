9cfc8a7efd9242d268974322f03fb0da29c9bf4d
andjey44 <andjeykh@yandex.ru>
2020-04-21 14:42:04 +0300

    example java error

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,21 @@
 package com.company;
 
-import static org.apache.commons.lang3.ArrayUtils.*;
 
-public class Application {
-      public static void main(String[] argv) {
-            String[] phrase = {"Hello"};
-            phrase = add(phrase, " ");
-            phrase = add(phrase, "world");
-            for (String word : phrase) {
-                  System.out.print(word);
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+public class drygoereshenie {
+      public static void main(String[] args) {
+            try (FileWriter writer = new FileWriter(namefile, false)) {
+                  {
+                        String text = "hello gold";
+                        writer.write(text);
+                        writer.append('\n');
+                        writer.append('E');
+                  }
+      catch(IOException ex){
+                        System.out.println(ex.getMessage());
+                  }
             }
-            System.out.println();
       }
+}
