12938f7e8f7fd4e7e06f5aeefa6f7227549f62b9
andjey44 <andjeykh@yandex.ru>
2020-04-21 14:32:36 +0300

    example maven

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,51 +1,14 @@
 package com.company;
-public interface Messenger{
-      public void sendMessage();
 
-      public void getMessage();
-} public class Telegram implements Messenger {
-      public void sendMessage() {
+import static org.apache.commons.lang3.ArrayUtils.*;
 
-            System.out.println("Отправляем сообщение в Telegram!");
+public class Application {
+      public static void main(String[] argv) {
+            String[] phrase = {"Hello"};
+            phrase = add(phrase, " ");
+            phrase = add(phrase, "world");
+            for (String word : phrase) {
+                  System.out.print(word);
+            }
+            System.out.println();
       }
-
-      public void getMessage() {
-            System.out.println("Читаем сообщение в Telegram!");
-      }
-} public class Viber implements Messenger {
-      public void sendMessage() {
-
-            System.out.println("Отправляем сообщение в Viber!");
-      }
-
-      public void getMessage() {
-            System.out.println("Читаем сообщение в Viber!");
-      }
-} public class Vk implements Messenger {
-      public void sendMessage() {
-
-            System.out.println("Отправляем сообщение в Vk!");
-      }
-
-      public void getMessage() {
-            System.out.println("Читаем сообщение в Vk!");
-      }
-} public class odnoclassniki implements Messenger {
-      public void sendMessage() {
-
-            System.out.println("Отправляем сообщение в odnoklassniki!");
-      }
-
-      public void getMessage() {
-            System.out.println("Читаем сообщение в odnoklassniki!");
-      }
-} public class vatsapp implements Messenger {
-      public void sendMessage() {
-
-            System.out.println("Отправляем сообщение в vatsapp!");
-      }
-
-      public void getMessage() {
-            System.out.println("Читаем сообщение в vatsapp!");
-      }
-}
