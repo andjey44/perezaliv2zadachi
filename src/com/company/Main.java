db2bb339cba4670b0b536c795c21bdec1fa8a7b2
andjey44 <andjeykh@yandex.ru>
2020-04-28 16:44:39 +0300

    2 zadacha is KR pravilnaya

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,42 +1,53 @@
 package com.company;
 
-
+import java.util.Scanner;
 import java.util.Arrays;
 import java.util.SortedSet;
 import java.util.TreeSet;
-import java.util.Scanner;
-public class Main  {
 
 
 
-
-
-                        public static void main(String args[]) {
-                              Scanner scanner = new Scanner(System.in);
-                              int a = scanner.nextInt();
-                              int[] array = new int[a];
-                              int[] array1 = new int[a];
-                              int min = getMin(array);
-                              System.out.println("Minimum Value is: " + min);
-                              for (int i = 0; i < min; i++) {
-                                    System.out.print(array[i] + " ");
-                                    array1[i+5]=array[i];
-                                    array[i]=array1[i];
-                              }
-                        }
-
-                        public static int getMin(int[] inputArray){
-                              int minValue = inputArray[0];
-                              for(int i=1;i<inputArray.length;i++){
-                                    if(inputArray[i] < minValue){
-                                          minValue = inputArray[i];
-                                    }
-                              }
-                              return minValue;
-                        }
-
-      }
-
-                  }
-
+public class Main
+{
+    public static int[] shiftArr(int[] inArr, int shift) {
+        while (shift > 0) {
+            int lastVar = inArr[inArr.length - 1];
+            for (int counter = 0; counter < inArr.length; counter++) {
+                int curVal = inArr[counter];
+                inArr[counter] = lastVar;
+                lastVar = curVal;
+            }
+            shift--;
+        }
+        return inArr;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Размерность массива: ");
+        int n = scanner.nextInt();
+        int[] testArr = new int[n];
+        System.out.println("Введи сам массив: ");
+        for(int i = 0; i < n; i++){
+            testArr[i] = scanner.nextInt();
+        }
+        int min = getMin(testArr);
+        System.out.println("Минимальное массива: " + min);
+        testArr = shiftArr(testArr, min);
+        System.out.println("Массив после сдвига на " + min + " элементов вправа: ");
+        for (int i = 0; i < testArr.length; i++) {
+            System.out.print(testArr[i] + " ");
+        }
+    }
+    public static int getMin(int[] inputArray){
+        int minValue = inputArray[0];
+        for(int i=1;i<inputArray.length;i++){
+            if(inputArray[i] < minValue){
+                minValue = inputArray[i];
+            }
+        }
+        return minValue;
+    }
 }
+
+
+
