1b3c00b9e041565c694ffa1dd7266e461972530e
andjey44 <andjeykh@yandex.ru>
2020-04-28 15:49:50 +0300

    1 zadacha is KR

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,42 @@
 package com.company;
 
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.*;
-public class drygoereshenie {
-      public static void main(String[] args) {
-            try (FileWriter writer = new FileWriter(namefile, false)) {
-                  {
-                        String text = "hello gold";
-                        writer.write(text);
-                        writer.append('\n');
-                        writer.append('E');
-                  }
-      catch(IOException ex){
-                        System.out.println(ex.getMessage());
-                  }
-            }
+import java.util.Arrays;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.Scanner;
+public class Main  {
+
+
+
+
+
+                        public static void main(String args[]) {
+                              Scanner scanner = new Scanner(System.in);
+                              int a = scanner.nextInt();
+                              int[] array = new int[a];
+                              int[] array1 = new int[a];
+                              int min = getMin(array);
+                              System.out.println("Minimum Value is: " + min);
+                              for (int i = 0; i < min; i++) {
+                                    System.out.print(array[i] + " ");
+                                    array1[i+5]=array[i];
+                                    array[i]=array1[i];
+                              }
+                        }
+
+                        public static int getMin(int[] inputArray){
+                              int minValue = inputArray[0];
+                              for(int i=1;i<inputArray.length;i++){
+                                    if(inputArray[i] < minValue){
+                                          minValue = inputArray[i];
+                                    }
+                              }
+                              return minValue;
+                        }
+
       }
+
+                  }
+
 }
